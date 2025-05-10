00b11932a83702149a84d20a7814152e15502e24
Sabrinabd <bouadisabrina02@gmail.com>
2025-05-10 17:17:02 +0200

    administration des cocktails

---
+++ src/app/views/admin-cocktails/views/admin-cocktails-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-cocktails-list',
+  imports: [],
+  template: `
+    <p>
+      admin-cocktails-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class AdminCocktailsListComponent {
+
+}
